ac8ff2437db39492291deb7d178eb572848601e2
Sebastian Heidinger <sebastian.heidinger89@gmail.com>
2013-10-19 15:29:14 +0200

    added async loading to testcase actionbartitle

--- catroidTest/src/org/catrobat/catroid/uitest/ui/activity/MainMenuActivityTest.java
+++ catroidTest/src/org/catrobat/catroid/uitest/ui/activity/MainMenuActivityTest.java
@@ -71,6 +71,7 @@ public class MainMenuActivityTest extends BaseActivityInstrumentationTestCase<Ma
 
 	@Override
 	public void tearDown() throws Exception {
+		Reflection.setPrivateField(ProjectManager.class, ProjectManager.getInstance(), "asynchronTask", true);
 		UtilFile.deleteDirectory(new File(Utils.buildProjectPath(projectNameWithBlacklistedCharacters)));
 		UtilFile.deleteDirectory(new File(Utils.buildProjectPath(projectNameWithWhitelistedCharacters)));
 		// normally super.teardown should be called last
@@ -80,6 +81,7 @@ public class MainMenuActivityTest extends BaseActivityInstrumentationTestCase<Ma
 	}
 
 	public void testCreateNewProject() {
+		Reflection.setPrivateField(ProjectManager.class, ProjectManager.getInstance(), "asynchronTask", false);
 		File directory = new File(Constants.DEFAULT_ROOT + "/" + testProject);
 		UtilFile.deleteDirectory(directory);
 		assertFalse("testProject was not deleted!", directory.exists());
