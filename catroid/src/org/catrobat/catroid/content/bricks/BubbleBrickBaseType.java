467ad0af50ecf612ebec2ae6a00df4a4c70df166
Franz Schreiner <grunztiger@gmail.com>
2013-12-17 13:43:50 +0100

    Adding of license text

--- catroid/src/org/catrobat/catroid/content/bricks/BubbleBrickBaseType.java
+++ catroid/src/org/catrobat/catroid/content/bricks/BubbleBrickBaseType.java
@@ -1,3 +1,25 @@
+/**
+ *  Catroid: An on-device visual programming system for Android devices
+ *  Copyright (C) 2010-2013 The Catrobat Team
+ *  (<http://developer.catrobat.org/credits>)
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *  
+ *  An additional term exception under section 7 of the GNU Affero
+ *  General Public License, version 3, is available at
+ *  http://developer.catrobat.org/license_additional_term
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.catrobat.catroid.content.bricks;
 
 import android.content.Context;
@@ -76,7 +98,13 @@ public abstract class BubbleBrickBaseType extends BrickBaseType implements OnCli
 	}
 
 	protected String getNormalizedText() {
-		String text = this.text.interpretString(sprite);
+		String text;
+		try {
+			text = this.text.interpretString(sprite);
+		} catch (ClassCastException classCastException) {
+			text = String.valueOf(this.text.interpretDouble(sprite));
+		}
+
 		String normalizedText = new String();
 
 		for (int index = 0; index < text.length(); index++) {
@@ -96,7 +124,7 @@ public abstract class BubbleBrickBaseType extends BrickBaseType implements OnCli
 		bubble.layout(0, 0, bubble.getMeasuredWidth(), bubble.getMeasuredHeight());
 
 		Bitmap bitmap = bubble.getDrawingCache();
-		ByteArrayOutputStream stream = new ByteArrayOutputStream();
+		ByteArrayOutputStream stream = new ByteArrayOutputStream(bitmap.getByteCount());
 		bitmap.compress(Bitmap.CompressFormat.PNG, 0, stream);
 		bubbleByteArray = stream.toByteArray();
 		try {
